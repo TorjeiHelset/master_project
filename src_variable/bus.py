Bug when taking derivative of delay fixed by using lists

src_variable/bus.py
@@ -226,7 +226,7 @@ class Bus:
                 return i, relative_length, next_idx
         return -1, 0, -1 # Road not found, stop updating...
 
-    def update_position(self, t, dt, speed, activation, length):
+    def update_position(self, t, dt, speed, activation, length, printing = False):
         '''
         Calculates the next position given the current position and the speed and the
         time step
@@ -246,10 +246,17 @@ class Bus:
 
         if self.at_stop:
             # print(f"t = {t}, bus is at busstop")
+            if printing:
+                print(f"Bus is at stop!")
+
             if self.remaining_stop_time > dt:
-                self.remaining_stop_time -= dt
+                if printing:
+                    print(f"Bus should wait for {self.remaining_stop_time} seconds, more than the next time step")
+                self.remaining_stop_time = self.remaining_stop_time - dt
                 # The bus does not move
             else:
+                if printing:
+                    print("The bus has waited long enough!")
                 moving_dt = dt - self.remaining_stop_time
                 self.remaining_stop_time = 0
                 self.at_stop = False
@@ -266,23 +273,60 @@ class Bus:
             length_of_next_stop = self.stop_lengths[self.next_stop]
 
             if activation >= 0.5:
-                # print(f"t = {t}, bus is allowed to cross the junction")
+                #print(f"t = {t}, bus is allowed to cross the junction")
                 # Bus can pass through the junction
 
                 if self.length_travelled + speed * dt >= length_of_next_stop:
+                    if printing:
+                        print("Bus should stop at the busstop in this time step")
+                        try:
+                            print(f"Length travelled verison: {self.length_travelled._version}")
+                        except:
+                            pass
+
+                        try:
+                            print(f"Version of timestep: {dt._version}")
+                        except:
+                            pass
+
+                        try:
+                            print(f"Version of speed: {speed._version}")
+                        except:
+                            pass
+
+                        try:
+                            print(f"Version of length: {length._version}")
+                        except:
+                            pass
+
+                        try:
+                            print(f"Version of length_of_next_stop: {length_of_next_stop._version}")
+                        except:
+                            pass
+
+                        try:
+                            print(f"Version of dt: {dt._version}")
+                        except:
+                            pass
+                        
                     # print(f"t = {t}, bus should stop at the busstop")
                     actual_dt = (length_of_next_stop - self.length_travelled)/speed
                     # The bus should stop at the next stop
                     self.at_stop = True
-                    self.remaining_stop_time = 30 - (dt - actual_dt)
+                    self.remaining_stop_time = 30 - (dt - actual_dt) # This might be requiring gradient
                     # Calculate delay time
                     if self.next_stop < len(self.times):
-                        self.delays[self.next_stop] = torch.max(torch.tensor(0.0), t + actual_dt - self.times[self.next_stop])
+                        # At least one stop left
+
+                        self.delays[self.next_stop] = self.delays[self.next_stop] + torch.max(torch.tensor(0.0), t + actual_dt - self.times[self.next_stop])
                         # inplace is fine
                         self.next_stop += 1
+
                     self.length_travelled = self.length_travelled + speed * actual_dt # Could set equal to length_of_next_stop, but
                     # then it would not be possible to differentiate
                 else:
+                    if printing:
+                        print(f"Bus should travel full distance of {speed*dt} meters")
                     # print(f"t = {t}, bus should travel full distance of {speed*dt} meters")
                     self.length_travelled = self.length_travelled + speed * dt
             else:
@@ -296,13 +340,15 @@ class Bus:
                         self.remaining_stop_time = 30 - (dt - actual_dt)
                         # Calculate delay time
                         if self.next_stop < len(self.times):
-                            self.delays[self.next_stop] = torch.max(torch.tensor(0.0), t + actual_dt - self.times[self.next_stop])
+                            self.delays[self.next_stop] = self.delays[self.next_stop] + torch.max(torch.tensor(0.0), t + actual_dt - self.times[self.next_stop])
                             self.next_stop += 1
                         self.length_travelled = self.length_travelled + speed * actual_dt
                     else:
                         self.length_travelled = self.length_travelled + speed * dt
 
                 else:
+                    if printing:
+                        print(f"Busshould stop at the junction!")
                     # Bus could be stopped at the junction
                     try:
                         self.length_travelled = self.length_travelled + torch.min(speed * dt, length)

src_variable/generate_kvadraturen.py
@@ -273,7 +273,7 @@ def create_junctions_small(v_strand_fw, v_strand_bw, h_w, tollbod_fw, tollbod_bw
             #  festningsgata to festningsgata - no trafficlights - shifted one index
             festning_jncs[i] = jn.Junction([festning_fw[i+1], festning_fw[i+2],
                                             festning_bw[i+1], festning_bw[i+2]],
-                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [trafficlight], [])
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
     festning_jncs.pop(2)
     return v_strand_jncs, h_w_jncs, tollbod_jncs, dronning_jncs, festning_jncs
 
@@ -319,7 +319,219 @@ def minimal_kvadraturen(T = 100):
     network = nw.RoadNetwork([road1, road2, road3, road4], [junction], T, optimizing = False)
     return network
 
-def generate_kvadraturen_full():
-    ...
+def create_junctions_wo_tl(v_strand_fw, v_strand_bw, h_w, tollbod_fw, tollbod_bw,
+                           elvegata_fw, elvegata_bw, dronning_fw, dronning_bw,
+                           festning_fw, festning_bw, lundsbro_fw, lundsbro_bw):
+    # Create the junctions
+    # Should probably also have an id...
 
-    return ...
+    # For Vestre strandgate:
+    # vestre strandgate 1 is in/out road - only connected at one end
+    # Junctions where vestre strandgate is connected
+    v_strand_jncs = [None] * 8
+    for i in range(8):
+        if i < 2:
+            # strandgate to strandgate - no trafficlights
+            v_strand_jncs[i] = jn.Junction([v_strand_fw[i], v_strand_fw[i+1],
+                                            v_strand_bw[i], v_strand_bw[i+1]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+
+        elif i == 2:
+            # strandgate to henrik Wergeland 4 way + traffic light
+            distribution = [[0.7, 0.0, 0.3],
+                            [0.0, 0.7, 0.3],
+                            [0.3, 0.3, 0.4]]
+
+            v_strand_jncs[i] = jn.Junction([v_strand_fw[i], v_strand_fw[i+1],
+                                            v_strand_bw[i], v_strand_bw[i+1],
+                                            h_w[0], h_w[1]], [0,3,4], [1,2,5],
+                                            distribution, [], [])
+
+        elif i < 6:
+            # strandgate to strandgate - trafficlight
+
+            v_strand_jncs[i] = jn.Junction([v_strand_fw[i], v_strand_fw[i+1],
+                                            v_strand_bw[i], v_strand_bw[i+1]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+        elif i==6:
+            # strandgate to tollbod 1 way + traffic light
+            distribution = [[1.0, 0.0],
+                            [0.0, 1.0],
+                            [0.3, 0.7]]
+            v_strand_jncs[i] = jn.Junction([v_strand_fw[i], v_strand_fw[i+1],
+                                            v_strand_bw[i], v_strand_bw[i+1],
+                                            tollbod_bw[0]], [0,3,4], [1,2],
+                                            distribution, [], [])
+        else:
+            # strandgate to dronningens gate + traffic light
+            v_strand_jncs[i] = jn.Junction([v_strand_fw[i], dronning_fw[0],
+                                            v_strand_bw[i], dronning_bw[0]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+
+    # For Henrik Wergelands:
+    # Junctions where henrik Wergeland is connected (and nt vestre strandgate)
+    h_w_jncs = [None] * 3
+    for i in range(3):
+        if i < 2:
+            # henrik Wergeland to henrik Wergeland - no trafficlights
+            h_w_jncs[i] = jn.Junction([h_w[i], h_w[i+1]],
+                                        [0], [1], [[1.0]], [], [])
+        if i == 2:
+            #henrik Wergeland to festningsgata 3/4 + traffic light
+            distribution = [[0.5, 0.5],
+                            [1.0, 0.0],
+                            [0.0, 1.0]]
+
+
+            h_w_jncs[i] = jn.Junction([h_w[i], festning_fw[2], festning_fw[3],
+                                       festning_bw[2], festning_bw[3]],
+                                       [0,1,4], [2,3], distribution, [], [])
+
+    # For Tollbodgata:
+    # Junctions where tollbodgata is connected (and not vestre strandgate or henrik Wergeland)
+    tollbod_jncs = [None] * 6
+    for i in range(6):
+        if i < 2:
+            # tollbodgata to tollbodgata - no trafficlights - 1way
+            tollbod_jncs[i] = jn.Junction([tollbod_bw[i], tollbod_bw[i+1]],
+                                            [1], [0], [[1.0]], [], [])
+        elif i == 2:
+            # tollbodgata to festningsgata 7/8 + no traffic light
+            distribution = [[0.1, 0.8, 0.0, 0.1],
+                            [0.1, 0.0, 0.8, 0.1],
+                            [0.5, 0.25, 0.25, 0.0]]
+
+            tollbod_jncs[i] = jn.Junction([tollbod_bw[i], festning_fw[6], festning_fw[7],
+                                           festning_bw[6], festning_bw[7], tollbod_fw[i+1],
+                                           tollbod_bw[i+1]], [1,4,6], [0,2,3,5],
+                                           distribution, [], [])
+        elif i < 5:
+            # tollbodgata to tollbodgata - no traffic light - 2way
+            tollbod_jncs[i] = jn.Junction([tollbod_fw[i], tollbod_fw[i+1],
+                                            tollbod_bw[i], tollbod_bw[i+1]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+        else:
+            # tollbodgata to elvegata + no traffic light
+            tollbod_jncs[i] = jn.Junction([tollbod_fw[i], elvegata_fw[0],
+                                            tollbod_bw[i], elvegata_bw[0]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+
+    # For Dronningens gate:
+    # Junctions where dronningens gate is connected (and not vestre strandgate, henrik Wergeland or tollbodgata)
+    dronning_jncs = [None] * 6
+    for i in range(6):
+        if i in [0,3,4]:
+            # dronningens gate to dronningens gate - no trafficlights
+            dronning_jncs[i] = jn.Junction([dronning_fw[i], dronning_fw[i+1],
+                                            dronning_bw[i], dronning_bw[i+1]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+
+        elif i == 1:
+            # dronningens gate to dronningens gate - trafficlight
+
+            dronning_jncs[i] = jn.Junction([dronning_fw[i], dronning_fw[i+1],
+                                            dronning_bw[i], dronning_bw[i+1]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+
+        elif i == 2:
+            # dronningens gate to festningsgata 8/9 + traffic light
+            distribution = [[0.7, 0.0, 0.15, 0.15],
+                            [0.0, 0.7, 0.15, 0.15],
+                            [0.15, 0.15, 0.7, 0.0],
+                            [0.15, 0.15, 0.0, 0.7]]
+
+            dronning_jncs[i] = jn.Junction([dronning_fw[i], dronning_fw[i+1],
+                                            dronning_bw[i], dronning_bw[i+1],
+                                            festning_fw[7], festning_fw[8],
+                                            festning_bw[7], festning_bw[8]],
+                                            [0,3,4,7], [1,2,5,6], distribution, [], [])
+        elif i == 5:
+            # dronningens gate to elvegata and lundsbro + traffic light
+            distribution = [[0.7, 0.0, 0.3],
+                            [0.0, 0.7, 0.3],
+                            [0.5, 0.5, 0.0]]
+
+            dronning_jncs[i] = jn.Junction([dronning_fw[i], lundsbro_fw[0],
+                                            dronning_bw[i], lundsbro_bw[0],
+                                            elvegata_fw[0], elvegata_bw[0]],
+                                            [0,3,4], [1,2,5], distribution, [], [])
+
+
+    # For Festningsgata:
+    # Junctions where festningsgata is connected (and not vestre strandgate, henrik Wergeland, tollbodgata or dronningens gate)
+    festning_jncs = [None] * 5
+    for i in range(5):
+        if i == 0:
+            # festningsgata to festningsgata - trafficlight
+            festning_jncs[i] = jn.Junction([festning_fw[i], festning_fw[i+1],
+                                            festning_bw[i], festning_bw[i+1]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+        elif i == 1:
+            # festningsgata to festningsgata - no trafficlight
+            festning_jncs[i] = jn.Junction([festning_fw[i], festning_fw[i+1],
+                                            festning_bw[i], festning_bw[i+1]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+        elif i == 3:
+            # festningsgata to festningsgata - trafficlight - shifted one index
+
+            festning_jncs[i] = jn.Junction([festning_fw[i+1], festning_fw[i+2],
+                                            festning_bw[i+1], festning_bw[i+2]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+        elif i in [4, 5]:
+            #  festningsgata to festningsgata - no trafficlights - shifted one index
+            festning_jncs[i] = jn.Junction([festning_fw[i+1], festning_fw[i+2],
+                                            festning_bw[i+1], festning_bw[i+2]],
+                                            [0,3], [1,2], [[1.0, 0.0],[0.0, 1.0]], [], [])
+    festning_jncs.pop(2)
+    return v_strand_jncs, h_w_jncs, tollbod_jncs, dronning_jncs, festning_jncs
+
+def generate_kvadraturen_wo_tl(T):
+    '''
+    In the first iteration, do things manually
+    This is rather unfeasible for the large case, but is okay in this smaller network
+    '''
+
+    # Create the roads
+    v_strand_fw, v_strand_bw, h_w, tollbod_fw, tollbod_bw, elvegata_fw, \
+    elvegata_bw, dronning_fw, dronning_bw, festning_fw, festning_bw, \
+    lundsbro_fw, lundsbro_bw = create_roads_small()
+
+    # Create the junctions
+    # Create the junctions
+    v_strand_jncs, h_w_jncs, tollbod_jncs, dronning_jncs, festning_jncs = create_junctions_wo_tl(v_strand_fw, v_strand_bw, h_w,
+                                                                                                 tollbod_fw, tollbod_bw, elvegata_fw,
+                                                                                                 elvegata_bw, dronning_fw, dronning_bw,
+                                                                                                 festning_fw, festning_bw, lundsbro_fw,
+                                                                                                 lundsbro_bw)
+
+    # Create the network
+    roads = v_strand_fw + v_strand_bw + h_w + tollbod_fw[3:] + tollbod_bw + elvegata_fw + \
+            elvegata_bw + dronning_fw + dronning_bw + festning_fw + festning_bw + lundsbro_fw + lundsbro_bw
+    junctions = v_strand_jncs + h_w_jncs + tollbod_jncs + dronning_jncs + festning_jncs
+
+    network = nw.RoadNetwork(roads, junctions, T, optimizing = False)
+
+    return network
+
+def generate_minimal(T):
+    # Only four roads, one junction
+    road1 = rd.Road(1, 50, 5, torch.tensor([50.0], requires_grad=True), [], left_pos=(-1, 0), right_pos=(0, 0),
+                    inflow = 0.1, id="road1")
+    road2 = rd.Road(1, 50, 5, torch.tensor([50.0], requires_grad=True), [], left_pos=(0, 0), right_pos=(1, 0),
+                    inflow = 0.1, id="road2")
+    road3 = rd.Road(1, 50, 5, torch.tensor([50.0], requires_grad=True), [], left_pos=(0, -1), right_pos=(0, 0),
+                    inflow = 0.1, id="road3")
+    road4 = rd.Road(1, 50, 5, torch.tensor([50.0], requires_grad=True), [], left_pos=(0, 0), right_pos=(0, 1),
+                    inflow = 0.1, id="road4")
+    
+    trafficlight = tl.CoupledTrafficLightContinuous(False, [0], [2], [1], [3], [50.0, 50.0])
+    junction = jn.Junction([road1, road2, road3, road4], [0,1], [2,3], [[1.0, 0.0],[0.0, 1.0]], [], [trafficlight])
+    
+    network = nw.RoadNetwork([road1, road2, road3, road4], [junction], T, optimizing = False)
+    return network
+
+# Can also have a network where most of the road speed limits do not require a gradient...
+
+
+
+    

src_variable/junction.py
@@ -300,8 +300,14 @@ class Junction:
             for j in range(m):
                 fluxes[i,j] = active[i,j]*self.distribution[i][j]*max_dens_in[i] * i_flux
         
-
+        
+        # Skip this part to check if error is here...
+        fluxes[0,0] = torch.tensor(0.4)
+                
         # calculate the capacity of each road j
+        # capacities = torch.zeros(m)
+        # sum_influxes = torch.zeros(m)
+
         for j in range(m):
             capacity = max_dens_out[j] * fv.S(rho_out[j].clone(),  gamma_out[j])
 
@@ -309,7 +315,9 @@ class Junction:
             sum_influx = torch.sum(fluxes[:,j])
             if sum_influx > capacity:
                 # If the sum of the fluxes is larger than the capacity, scale down the fluxes
-                fluxes[:,j] = fluxes[:,j] * capacity / sum_influx
+                # fluxes[:,j] = fluxes[:,j] * capacity / sum_influx
+                for i in range(n):
+                    fluxes[i,j] = fluxes[i,j] * capacity / sum_influx
 
 
         fluxes_in = [0]*n
@@ -340,6 +348,97 @@ class Junction:
         for j in range(m):
             # Scaling flux back to correspond to maximum density equal to 1
             fluxes_out[j] = torch.sum(fluxes[:,j]) / max_dens_out[j]
+            # fluxes_out[j] = sum([fluxes[i][j] for i in range(n)]) / max_dens_out[j]
+        
+        return fluxes_in, fluxes_out
+    
+
+    def divide_flux_wo_opt_list(self, t):
+        # Is all of the below necessary? 
+        # road_in = [self.roads[i] for i in self.entering]
+        # road_out = [self.roads[i] for i in self.leaving]
+
+        rho_in = [road.rho[-road.pad] for road in self.road_in]
+        gamma_in = [road.gamma[road.idx] for road in self.road_in]
+        max_dens_in = torch.tensor([road.max_dens for road in self.road_in])
+        rho_out = [road.rho[road.pad-1] for road in self.road_out]
+        gamma_out = [road.gamma[road.idx] for road in self.road_out]
+        max_dens_out = torch.tensor([road.max_dens for road in self.road_out])
+
+
+        # If sentences can be moved outside of j for loop and split into two if-sentences
+        # Would reducde the number of times the if-sentence is evaluated
+        n = len(self.entering)
+        m = len(self.leaving)
+        # active = torch.ones((n,m))
+        active = [[torch.tensor(0.0) for _ in range(m)] for _ in range(n)]
+
+        # Probably quicker - need to check that the functionality is the same
+        for light in self.trafficlights:
+            for i in range(n):
+                if self.entering[i] in light.entering:
+                    for j in range(m):
+                        if self.leaving[j] in light.leaving:
+                            active[i][j] = light.activation_func(t)
+
+        
+        for light in self.coupled_trafficlights:
+            for i in range(n):
+                if self.entering[i] in light.a_entering:
+                    for j in range(m):
+                        if self.leaving[j] in light.a_leaving:
+                            active[i][j] = light.a_activation(t)
+
+                if self.entering[i] in light.b_entering:
+                    for j in range(m):
+                        if self.leaving[j] in light.b_leaving:
+                            active[i][j] = light.b_activation(t)
+        
+        # fluxes[i,j] is the flux from road i to road j
+        fluxes = [[torch.tensor(0.0) for _ in range(m)] for _ in range(n)]
+
+        # Calculate the desired flux from each road i to road j
+        for i in range(n):
+            # move D to be here to reduce the number of calls
+            i_flux = fv.D(rho_in[i].clone(), gamma_in[i])            
+            for j in range(m):
+                fluxes[i][j] = active[i][j]*self.distribution[i][j]*max_dens_in[i] * i_flux
+        
+        
+        # Skip this part to check if error is here...
+
+        for j in range(m):
+            capacity = max_dens_out[j] * fv.S(rho_out[j].clone(),  gamma_out[j])
+
+            # Update the flux from all roads into road j
+            sum_influx = torch.tensor(0.0)
+            for i in range(n):
+                sum_influx = sum_influx +  fluxes[i][j]
+
+            if sum_influx > capacity:
+                # If the sum of the fluxes is larger than the capacity, scale down the fluxes
+                # fluxes[:,j] = fluxes[:,j] * capacity / sum_influx
+                for i in range(n):
+                    fluxes[i][j] = fluxes[i][j] * capacity / sum_influx
+
+
+        fluxes_in = [0]*n
+        fluxes_out = [0]*m
+
+        for i in range(n):
+            # Scaling flux back to correspond to maximum density equal to 1
+            influx = torch.tensor(0.0)
+            for j in range(m):
+                influx = influx + fluxes[i][j]
+            fluxes_in[i] = influx / max_dens_in[i]
+
+
+        for j in range(m):
+            # Scaling flux back to correspond to maximum density equal to 1
+            outflux = torch.tensor(0.0)
+            for i in range(n):
+                outflux = outflux + fluxes[i][j]
+            fluxes_out[j] = outflux / max_dens_out[j]
             # fluxes_out[j] = sum([fluxes[i][j] for i in range(n)]) / max_dens_out[j]
         
         return fluxes_in, fluxes_out
@@ -423,7 +522,8 @@ class Junction:
         # road_in = [self.roads[i] for i in self.entering]
         # road_out = [self.roads[i] for i in self.leaving]
 
-        fluxes_in, fluxes_out = self.divide_flux_wo_opt(t)
+        # fluxes_in, fluxes_out = self.divide_flux_wo_opt(t)
+        fluxes_in, fluxes_out = self.divide_flux_wo_opt_list(t)
         # print(f"influx/outflux: {fluxes_in}, {fluxes_out}")
 
         # Ideally want to reduce the number of calls to d_flux and flux
@@ -547,17 +647,20 @@ class Junction:
             for light in self.trafficlights:
                 if in_idx in light.entering and out_idx in light.leaving:
                     activation = light.activation_func(t)
+                    # print("Regular traffic light")
                     return True, activation
 
             for light in self.coupled_trafficlights:
                 if in_idx in light.a_entering and out_idx in light.a_leaving:
                     activation = light.a_activation(t)
+                    # print("Coupled traffic light, state a")
                     return True, activation
 
                 if in_idx in light.b_entering and out_idx in light.b_leaving:
                     activation = light.b_activation(t)
+                    # print("Coupled traffic light, state b")
                     return True, activation
-
+        # print("No traffic light")
         return True, activation
     
     def get_speed(self, t, id_1, id_2):
@@ -634,8 +737,8 @@ class Junction:
             # print(f"Total sum of fluxes into road {j}: {sum_influx}")
             if sum_influx > capacity:
                 # If the sum of the fluxes is larger than the capacity, scale down the fluxes
-                fluxes[:,j] = fluxes[:,j] * capacity / sum_influx
-
+                fluxes[:,j] = fluxes[:,j] * capacity / sum_influx # is this the inplace in question?
+                
         flux = fluxes[idx_1, idx_2] / max_dens_in[idx_1]
         speed = flux / rho_in[idx_1] * self.road_in[idx_1].L
         return speed

src_variable/network.py
@@ -33,7 +33,7 @@ class RoadNetwork:
     optimizing = True
 
     def __init__(self, roads, junctions, T, busses = [], debugging = False, object_type = 0, iters = 1, dt = 0.001,
-                 optimizing = False, store_densities = True):
+                 optimizing = False, store_densities = True, print_control_points = False):
         # Check that unit length and dx are equal for all roads
         for i in range(1, len(roads)):
             assert roads[i].L == roads[i-1].L 
@@ -49,6 +49,7 @@ class RoadNetwork:
         self.debug_dt = dt
         self.optimizing = optimizing
         self.store_densities = store_densities
+        self.print_control_points = print_control_points
 
 
         # Update gamma for each road
@@ -230,11 +231,14 @@ class RoadNetwork:
 
         Solves model of road untill time T
         '''
+        printing = False
         
-        t = 0
+        t = torch.tensor(0)
         if self.store_densities:
             rho_timesteps = {i : {0 : self.roads[i].rho} for i in range(len(self.roads))}
-            queue_timesteps = {i : {0 : self.roads[i].queue_length.clone()} for i in range(len(self.roads))}
+            # queue_timesteps = {i : {0 : self.roads[i].queue_length.clone()} for i in range(len(self.roads))}
+            queue_timesteps = {i : {0 : self.roads[i].queue_length} for i in range(len(self.roads))}
+
         else:
             rho_timesteps = {i : {} for i in range(len(self.roads))}
             queue_timesteps = {i : {} for i in range(len(self.roads))}
@@ -266,12 +270,17 @@ class RoadNetwork:
                 # print(f"road index {road.idx}")
                 # print(f"Speed limit on road {road.Vmax[road.idx]}")
                 # print(f"Gamma parameter {road.gamma[road.idx]}")
-
+            if self.print_control_points:
+                print("\n-----------------------------------------")
+                print(f"Controlpoint: {controlpoint}")
+                print("-----------------------------------------\n")
             while t < controlpoint:
                 #-------------------------------------
                 # STEP 1: Find appropriate timestep
                 #-------------------------------------                
-                dt = torch.tensor(controlpoint - t)
+                # dt = torch.tensor(controlpoint - t)
+                dt = controlpoint - t
+                
 
                 for road in self.roads:
                     dt = torch.min(dt, road.max_dt())
@@ -282,13 +291,20 @@ class RoadNetwork:
                 #dt = min(dt, 1/1280)
             
                 t = t + dt
+                # if t > 190:
+                #     # Print to find potential error location
+                #     printing = True
+                #     print(f"t = {t}, dt = {dt}")
 
                 #-------------------------------------
                 # STEP 2: Update positions of busses
                 #-------------------------------------
-                for bus in self.busses:
+                for i, bus in enumerate(self.busses):
                     # 1. Find the road the bus is on
                     road_id, length, next_id = bus.get_road_id()
+                    if printing:
+                        print(f"Bus {i} is on road {road_id} at length {length} and next road is {next_id}")
+
                     # print(f"Current length travelled: {bus.length_travelled}")
                     # print(f"Current length travelled on road: {length}")
                     # print("Current and next road:")
@@ -304,17 +320,24 @@ class RoadNetwork:
                     # Add some check on the length here
                     if next_id == "":
                         # Road_id is at the last road, don't need to find the next junction
-                        activation = 1.0
+                        activation = torch.tensor(1.0)
                     else:
                         # Find the junction and the activation function of the two roads
                         # If the activation is above i.e. 0.5, the bus can cross the junction
                         # If the activation is below, the bus cannot go further than the junction
-                        activation = 1.0 # Replace with actual
+                        activation = torch.tensor(1.0) # Replace with actual
                         for j in self.junctions:
                             check, activ_ = j.get_activation(t, road_id, next_id)
                             if check:
                                 activation = activ_
                                 break
+
+                    if printing:
+                        print(f"Activation function: {activation}")
+                        try:
+                            print(f"Version of activation function: {activation._version}")
+                        except:
+                            pass
                     # if activation < 0.5:
                     #     # Red light between the two roads
                     #     print("\n###############################################")
@@ -328,6 +351,8 @@ class RoadNetwork:
                     road = self.get_road(road_id)
                     # print("Speed of bus")
                     if length >= road.L*road.b:
+                        if printing:
+                            print("calculating speed using the junction")
                         # Bus has reached the end of the road
                         # print("Bus has reached the end of the road")
                         # print("Bus is at the junction!")
@@ -337,14 +362,22 @@ class RoadNetwork:
                         else:
                             speed = torch.tensor(0.0)
                     else:
-                        speed = road.get_speed(length/road.L) * road.L # Need to multiply with L to get actual speed in m/s
+                        if printing:
+                            print("calculating speed using nodes of road")
+                        # print("Calculating speed using nodes of road")
+                        new_length = length / road.L
+                        speed = road.get_speed(new_length, printing) * road.L # Need to multiply with L to get actual speed in m/s
                     # print(speed)
                     # prev_length = bus.length_travelled
                     # if activation < 0.5:
                     #     prev_length = bus.length_travelled.clone()
                     # print(f"t = {t}, activation = {activation}")
                     # print(f"t = {t},speed = {speed}, dt = {dt}, activation = {activation}, road length = {road.L*road.b}, bus length = {length}, road id = {road_id}")
-                    bus.update_position(t, dt, speed, activation, road.L*road.b - length)
+                    if printing:
+                        print(f"Speed of bus is {speed}")
+
+                    relative_length = road.L*road.b - length
+                    bus.update_position(t, dt, speed, activation, relative_length, printing=printing)
                     # print(f"New length travelled: {bus.length_travelled}")
                     # if activation < 0.5:
                     #     print(f"New length on road: {bus.length_travelled - prev_length}" )
@@ -352,7 +385,7 @@ class RoadNetwork:
                 #-------------------------------------
                 # STEP 3: Solve internal system for each road
                 #-------------------------------------
-                
+                # if t < 1000:
                 for road in self.roads:
                     # Solve internally on all roads in network
                     # Before updating internal values, values near boundary should maybe be saved to 
@@ -360,9 +393,11 @@ class RoadNetwork:
                     # Right now values near boundary at next time step is used to update boundary.
                     road.solve_internally(dt)
 
+
                 #-------------------------------------
                 # STEP 4: Apply flux conditions for each Junction
                 #-------------------------------------
+                # if t < 1000:
                 if self.optimizing:
                     for J in self.junctions:
                         # Apply boundary conditions to all junctions
@@ -374,11 +409,22 @@ class RoadNetwork:
                 #-------------------------------------
                 # STEP 5: Apply BC to roads with one or more edges not connected to junction
                 #-------------------------------------
+                # if t < 1000:
                 for road in self.roads:
                     # Add boundary conditions to remaining roads
                     road.apply_bc(t, dt)
 
                 
+                #-------------------------------------
+                # STEP 5.5: Check version of densities
+                #-------------------------------------
+                # if t > 195:
+                #     for i, road in enumerate(self.roads):
+                #         for j, rho in enumerate(road.rho):
+                #             print(f"Density of node {i} on road {j} has version {rho._version}")
+
+
+                # Is cycle times being updated?????
 
                 #-------------------------------------
                 # STEP 6: Store solution after time t
@@ -388,10 +434,14 @@ class RoadNetwork:
                 if self.store_densities:
                     for i in range(len(self.roads)):
                         rho_timesteps[i][t] = self.roads[i].rho
-                        queue_timesteps[i][t] = self.roads[i].queue_length.clone()
+                        # queue_timesteps[i][t] = self.roads[i].queue_length.clone()
+                        queue_timesteps[i][t] = self.roads[i].queue_length
+
 
                 for i in range(len(self.busses)):
+                    # bus_timesteps[i][t] = self.busses[i].length_travelled.clone()
                     bus_timesteps[i][t] = self.busses[i].length_travelled.clone()
+
 
                 if self.debugging:
                     i_count += 1
@@ -400,7 +450,8 @@ class RoadNetwork:
                         t = self.T+1
 
                 
-
+                if printing:
+                    print("-----------------------------------------\n")
                 # if self.debugging:
                 #     t = T+1 # Only do 1 iteration for debugging purposes
         #-------------------------------------

src_variable/road.py
@@ -402,7 +402,7 @@ class Road:
                 # (i+1)th time interval
                 return self.gamma[i]
         
-    def get_speed(self, length):
+    def get_speed(self, length, printing = False):
         '''
         Return the speed calculated at a given length
         For now, just use local speed
@@ -427,16 +427,37 @@ class Road:
         # print(pos)
 
         prev = torch.floor(pos)
-        next = torch.floor(pos+1.0)
+        next = prev + 1
 
+        if printing:
+            print(f"Position: {pos}")
+            print(f"Nodes: {prev} and {next}")
+            print(f"Closeness to nodes: {1 + prev - pos} and {1 + pos - next}")
+            print(f"Speed: {self.gamma[self.idx]}")
+        
         prev_speed = self.gamma[self.idx] * (1. - self.rho[int(prev)])
         next_speed = self.gamma[self.idx] * (1. - self.rho[int(next)])
+        if printing:
+            try:
+                print(f"Version of left density: {self.rho[int(prev)]._version}, {self.rho[int(prev)]}")
+            except:
+                pass
 
-        avg_speed = (1 + prev - pos) * prev_speed + (1 + pos - next) * next_speed
-        # print(f"Nodes: {prev} and {next}")
-        # print(f"Closeness to nodes: {1 + prev - pos} and {1 + pos - next}")
-        # print(f"Densities: {self.rho[int(prev)]} and {self.rho[int(next)]}")
-        # print(f"Speeds: {prev_speed} and {next_speed}")
-        # print(f"Average speed: {avg_speed}")
+            try:
+                print(f"Version of right density: {self.rho[int(next)]._version}, {self.rho[int(next)]}")
+            except:
+                pass
+
+            print(type(self.rho[int(prev)]))
+        
+        if printing:
+            print(f"Speeds: {prev_speed} and {next_speed}")
+            print(f"Densities: {self.rho[int(prev)]} and {self.rho[int(next)]}")
+            print(f"Speed limit: {self.Vmax[self.idx]}, version {self.Vmax[self.idx]._version}")
+        
+        avg_speed = (1. + prev - pos) * prev_speed + (1. + pos - next) * next_speed
+        
+        if printing:
+            print(f"Average speed: {avg_speed}")
 
         return avg_speed