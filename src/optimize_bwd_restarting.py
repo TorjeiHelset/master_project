5bb1867207faf1705c4a0ebfe50b5a53b5c34201
TorjeiHelset <torjei.helset@gmail.com>
2024-05-27 10:57:44 +0200

    Changed output filename of option 4 and 5 in bwd and restarting optimization

--- src/optimize_bwd_restarting.py
+++ src/optimize_bwd_restarting.py
@@ -611,13 +611,13 @@ if __name__ == "__main__":
         case 4:
             network_file = "kvadraturen_networks/with_e18/network_3.json"
             config_file = "kvadraturen_networks/with_e18/config_3_1.json"
-            result_file = "optimization_results/network3_config31_fwd.json"
+            result_file = "optimization_results/network3_config31_restart.json"
             gradient_descent(network_file, config_file, result_file,
                              overwrite=False, debugging=False)
             
         case 5:
             network_file = "kvadraturen_networks/with_e18/network_4.json"
             config_file = "kvadraturen_networks/with_e18/config_4_1.json"
-            result_file = "optimization_results/network4_config41_fwd.json"
+            result_file = "optimization_results/network4_config41_restart.json"
             gradient_descent(network_file, config_file, result_file,
                              overwrite=False, debugging=False)
